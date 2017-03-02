3c7f5ef7bddd54526c840111dd664585ed6b43c7
hermaneric5605 <herman7279@gmail.com>
2017-03-02 13:25:24 -0500

    stack overflow solution

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,7 +1,27 @@
+def merge_sort(arr)
+  # 0. Base case
+  return arr if arr.length <= 1
 
+  # Divide
+  mid = arr.length / 2
+  arr0 = merge_sort(arr[0, mid])
+  arr1 = merge_sort(arr[mid, arr.length])
 
-def merge_sort(array)
-	return array if array.length == 1
-	mid = array.length/2
-
+  # Conquer
+  output = merge(arr0, arr1)
 end
+
+def merge(l, r)
+  output = []
+  until l.length == 0 || r.length == 0
+    output << if l[0] <= r[0]
+                l.shift
+              else
+                r.shift
+              end
+  end
+  # The order of `concat(l)` or `concat(r)` does not matters
+  output.concat(l).concat(r)
+end
+
+merge_sort([11, 4, 2, 15, 9, 21])
